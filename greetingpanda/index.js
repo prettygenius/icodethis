dce370c00ee908a829e09b5906bf684a8b8d8901
prettygenius <devprettygenius@gmail.com>
2025-04-15 11:42:27 -0500

    notes

--- greetingpanda/index.js
+++ greetingpanda/index.js
@@ -1,7 +1,3 @@
-// <div class="chat-bubble">
-//<p class="message">Hi, <span id="name" class="name">iCoder</span>!</p>
-//</div>
-
 const userName = document.getElementById('name')
 
 userName.textContent = 'Ashlee'
